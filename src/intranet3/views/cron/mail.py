d34308c5683cbf26a3e2c3ce881a2c52d440b570
meverone <meverone@gmail.com>
2013-09-06 15:42:08 +0200

    Delete mark_not_busy()

--- src/intranet3/views/cron/mail.py
+++ src/intranet3/views/cron/mail.py
@@ -18,33 +18,22 @@ DEBUG = DEBUG_LOG(__name__)
 @view_config(route_name="cron_mail_get")
 class Sync(CronView):
 
-    MAX_BUSY_CALLS = 3
     POP3_SERVER = 'pop.gmail.com'
     POP3_PORT = 995
     context_factory = ssl.ClientContextFactory()
 
-    def mark_not_busy(self):
-        if not self.busy:
-            WARN(u'Tried to unmark an already unmarked Mail Checker')
-        else:
-            self.busy = False
-            LOG(u'Marked Mail Check as not busy anymore')
-
     def action(self):
-        self.busy = False
-        self.busy_calls = 0
-
         config = ApplicationConfig.get_current_config(allow_empty=True)
         if config is None:
             WARN(u'Application config not found, emails cannot be checked')
-            return self.mark_not_busy()
+            return Response("Application config not found.")
         trackers = dict(
             (tracker.mailer, tracker)
                 for tracker in Tracker.query.filter(Tracker.mailer != None).filter(Tracker.mailer != '')
         )
         if not len(trackers):
             WARN(u'No trackers have mailers configured, email will not be checked')
-            return self.mark_not_busy()
+            return Response('No trackers have mailers configured, email will not be checked')
 
         username = config.google_user_email.encode('utf-8')
         password = config.google_user_password.encode('utf-8')
@@ -66,9 +55,10 @@ class Sync(CronView):
         )
 
         # all pre-conditions should be checked by now
-
+        def empty_callback():
+            pass
         # start fetching
-        f = CustomClientFactory(username, password, self.mark_not_busy,
+        f = CustomClientFactory(username, password, empty_callback,
             trackers, logins_mappings, projects, selector_mappings)
         f.protocol = MailerPOP3Client
         reactor.connectSSL(self.POP3_SERVER, self.POP3_PORT, f, self.context_factory)
